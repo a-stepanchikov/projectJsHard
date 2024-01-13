2f14895b80c95e0abbdaa13c58702f163ab44f6a
a-stepanchikov <aleksrims@gmail.com>
2024-01-13 22:04:41 +0300

    complete fix hard js lesson02

---
+++ scriptHard.js
@@ -0,0 +1,19 @@
+let num = 266219;
+
+num = String(num);
+
+let comp = num[0] * num[1] * num[2] * num[3] * num[4] * num[5];
+
+console.log (comp);
+
+let degree = comp ** 3;
+
+console.log (degree);
+
+degree = String(degree);
+
+console.log (degree.substring(0, 2));
+
+
+
+
